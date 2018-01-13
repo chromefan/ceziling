a6a4bd0aab6166b42812020791d6d4bd217c1e56
luohuanjun <luohuanjun@dingfu.com>
2018-01-13 17:34:07 +0800

    忽略

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
 <?php
-define('SITE_PATH', getcwd());
-define('APP_NAME', 'home');
-define('APP_PATH', './home/');
-define('APP_DEBUG', true);
-define('REWRITE_ON', true); //打开REWRITE_ON后 __APP__常量将不包含index.php，须与URL_MODEL配合使用
 
-define ( "GZIP_ENABLE", function_exists ( 'ob_gzhandler' ) );
-ob_start ( GZIP_ENABLE ? 'ob_gzhandler' : '');
-$_SGLOBAL=array();
-require( "./ThinkPHP/ThinkPHP.php");
 ?>
